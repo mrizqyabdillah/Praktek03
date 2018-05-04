0536c6b043429cbee869ab176601c2a86fdbb282
Muhammad Rizqy Abdillah <34591230+mrizqyabdillah@users.noreply.github.com>
2018-05-04 12:01:28 +0800

    memanggil method

--- src/praktek03/SepedaAksi.java
+++ src/praktek03/SepedaAksi.java
@@ -6,6 +6,7 @@ public class SepedaAksi {
         s.setMerk("Hornet");
         s.setWarna("Hitam");
         s.setHarga(100000);
+        //gak bisa s.setJenisMotor();
         
         s.cetakInfo();
         
@@ -15,5 +16,14 @@ public class SepedaAksi {
         System.out.println(s.getWarna());
         System.out.print("Merknya \t: ");
         System.out.println(s.getHarga());
+        
+        SepedaMotor sm = new SepedaMotor();
+        sm.setMerk("Lanaya");
+        sm.setWarna("Hitam");
+        sm.setHarga(150000);
+        sm.setJenisMotor("Aki");
+        sm.cetakInfo();
+        System.out.println("Jenis :"+sm.getJenisMotor());
+        sm.throttle();
     }
 }
